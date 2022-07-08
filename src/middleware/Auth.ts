7ff1a8847d26fc677d363bc48f049eab0d1f266d
noecende <noecende@gmail.com>
2022-07-07 23:19:47 -0500

    :heavy_minus_sign: Se quitaron dependencias que no se usan.

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -1,8 +1,5 @@
 import { AuthenticationError } from "apollo-server-core";
-import { GraphQLError } from "graphql";
 import { MiddlewareFn } from "type-graphql";
-import Container from "typedi";
-import { AuthService } from "../services/AuthService";
 
 export const apiAuth: MiddlewareFn = async ({ context, info }, next) => {
     if(!context["user"]) {
