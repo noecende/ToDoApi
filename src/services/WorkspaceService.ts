474a29e1355dc1ee7248701463db95f127e4c495
noecende <noecende@gmail.com>
2022-07-08 00:07:39 -0500

    :construction: Se agregó el método owner.

--- src/services/WorkspaceService.ts
+++ src/services/WorkspaceService.ts
@@ -13,6 +13,21 @@ export class WorkspaceService {
         return await this.prisma.task.findUnique({where: {id: taskId}}).workspace();
     }
 
+    /**
+     * Obtiene el propietario de un espacio de trabajo por workspaceId
+     * @param {number} workspaceId - número
+     * @returns {User} El usuario propietario del viaje.
+     */
+    public async getOwner(workspaceId: number): Promise<User> {
+
+        let usersOnWorskpaces = await this.prisma
+                            .workspace
+                            .findUnique({ where: { id: workspaceId } })
+                            .participants({ where: { role: 'owner' }, include: { user: true } })
+                           
+        return usersOnWorskpaces[0].user 
+    }
+
     public async getParticipants(workspaceId: number) {
         return (await (this.prisma.workspace.findUnique({where: {id: workspaceId}}).participants({include: {user: true}}))).map(
             (relation: UsersOnWorkspaces & {user: User}) => {
