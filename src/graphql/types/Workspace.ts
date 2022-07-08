9bff668ad929544d4d54ba48d9bae19fa19656e0
noecende <noecende@gmail.com>
2022-07-07 23:08:23 -0500

    :fire: Se agregó type Workspace.

---
+++ src/graphql/types/Workspace.ts
@@ -0,0 +1,19 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import { Task } from "./Task";
+import { User } from "./User";
+
+@ObjectType()
+export class Workspace {
+
+    @Field(type => ID)
+    id: number
+
+    @Field()
+    name: String
+
+    @Field(type => [User])
+    participants: User[]
+
+    @Field(type => [Task])
+    tasks: Task[]
+}
