24d8f3dbe3ef8170150ca319c946aa716ff6d5a7
noecende <noecende@gmail.com>
2022-07-07 23:23:47 -0500

    :sparkles: Se agregaron operaciones para Workspace.

---
+++ src/graphql/resolvers/WorkspaceResolver.ts
@@ -0,0 +1,44 @@
+import { Arg, Args, Ctx, FieldResolver, Mutation, Query, Resolver, Root, UseMiddleware } from "type-graphql";
+import { Inject, Service } from "typedi";
+import { apiAuth } from "../../middleware/Auth";
+import { TaskService } from "../../services/TaskService";
+import { WorkspaceService } from "../../services/WorkspaceService";
+import { CreateWorkspaceArgs } from "../args/workspace/CreateWorkspaceArgs";
+import { WorkspaceArgs } from "../args/workspace/WorkspaceArgs";
+import { Task } from "../types/Task";
+import { Workspace } from "../types/Workspace";
+import { User } from "../types/User";
+
+@Service()
+@Resolver(Workspace)
+export class WorkspaceResolver {
+
+    constructor(
+        @Inject() private workspaceService: WorkspaceService,
+        @Inject() private taskService: TaskService
+        ) {}
+
+    @Query(returns => Workspace)
+    async workspace(@Args() {id}: WorkspaceArgs) {
+        return this.workspaceService.findById(id)
+    }
+
+    @Mutation(returns => Workspace)
+    @UseMiddleware(apiAuth)
+    async createWorkspace(
+        @Args() {name}: CreateWorkspaceArgs,
+        @Ctx() ctx: any) {
+        return this.workspaceService.createWorkspace(name, ctx.user.id)
+    }
+
+    @FieldResolver(returns => [Task])
+    async tasks(@Root() root: Workspace) {
+       return this.taskService.findByWorkspace(root.id)
+    }
+
+    @FieldResolver(returns => [User])
+    async participants(@Root() root: Workspace) {
+        return this.workspaceService.getParticipants(root.id)
+    }
+
+}
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,9 +4,10 @@ import Container from "typedi"
 import { AuthResolver } from "./resolvers/AuthResolver"
 import { TaskResolver } from "./resolvers/TaskResolver"
 import { UserResolver } from "./resolvers/UserResolver"
+import { WorkspaceResolver } from "./resolvers/WorkspaceResolver"
 
 export const schema = buildSchema({
-    resolvers: [TaskResolver, UserResolver, AuthResolver],
+    resolvers: [TaskResolver, UserResolver, AuthResolver, WorkspaceResolver],
     pubSub: new PubSub(),
     dateScalarMode: "isoDate",
     container: Container
